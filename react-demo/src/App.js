f95e287404aa74eb4275852ce3519dbc498e507c
Hasan <hasan.khan.pro@gmail.com>
2019-07-29 21:48:58 +0600

    new

---
+++ react-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ClickCounter from './components/ClickCounter'
+
+function App() {
+  return (
+    <div className="App">
+      <ClickCounter />
+    </div>
+  );
+}
+
+export default App;
